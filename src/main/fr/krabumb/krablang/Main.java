08e4e8738971de8a50d456beaf591a6281320d12
Clement LOSCOT <clement.loscot@inetum.com>
2022-06-10 14:16:30 +0200

    Ajout début projet

---
+++ src/main/fr/krabumb/krablang/Main.java
@@ -0,0 +1,11 @@
+package fr.krabumb.krablang;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("usage : krablang.jar <file>");
+            System.exit(1);
+        }
+
+    }
+}
---
+++ src/main/fr/krabumb/krablang/analex/AnalyseSemantique.java
@@ -0,0 +1,4 @@
+package fr.krabumb.krablang.analex;
+
+public class AnalyseSemantique {
+}
